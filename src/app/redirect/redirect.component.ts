a5ab0552400d1acdbfc9eff2d71553ef31b51187
damienbod <damien_bod@hotmail.com>
2017-09-01 09:15:21 +0200

    Setting up the redirect for 65328/redirect.html, fixed for the demo Azure AD STS

--- src/app/redirect/redirect.component.ts
+++ src/app/redirect/redirect.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   template: '<p>Signing in...</p>'
 })
-export class RedirectComponent implements OnInit {
+export class RedirectComponent implements OnInit, OnDestroy {
 
-  constructor(private oidcSecurityService: OidcSecurityService, private router: Router) {
-  }
+	constructor(private oidcSecurityService: OidcSecurityService, private router: Router) {
+	  if (this.oidcSecurityService.moduleSetup) {
+			this.doCallbackLogicIfRequired();
+		} else {
+			this.oidcSecurityService.onModuleSetup.subscribe(() => {
+				this.doCallbackLogicIfRequired();
+			});
+		}
+	}
 
-  ngOnInit() {
-    this.oidcSecurityService.authorizedCallback();
+	ngOnInit() {
 
-    this.router.navigate(['/home']);
-  }
+	//this.router.navigate(['/home']);
+	}
+
+	ngOnDestroy(): void {
+		this.oidcSecurityService.onModuleSetup.unsubscribe();
+	}
+
+	private doCallbackLogicIfRequired() {
+		if (window.location.hash) {
+			this.oidcSecurityService.authorizedCallback();
+		}
+	}
 }
