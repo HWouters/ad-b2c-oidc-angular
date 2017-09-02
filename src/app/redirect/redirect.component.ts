678fa3d5e4afc1cd798acf30e54879797f014ba6
Hendrik Wouters <hendrik.wouters@live.nl>
2017-09-02 20:54:11 +0200

    Review redirect fix.

--- src/app/redirect/redirect.component.ts
+++ src/app/redirect/redirect.component.ts
@@ -1,41 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   template: '<p>Signing in...</p>'
 })
-export class RedirectComponent implements OnInit, OnDestroy {
+export class RedirectComponent implements OnInit {
 
-	constructor(private oidcSecurityService: OidcSecurityService, private router: Router) {
-		console.log("redirect constructor");
-	  if (this.oidcSecurityService.moduleSetup) {
-		  console.log("redirect constructor moduleSetup");
-			this.doCallbackLogicIfRequired();
-		} else {
-			console.log("redirect constructor subscribe");
-			this.oidcSecurityService.onModuleSetup.subscribe(() => {
-				this.doCallbackLogicIfRequired();
-			});
-		}
-	}
+  constructor(private oidcSecurityService: OidcSecurityService) {
+  }
 
-	ngOnInit() {
-
-	console.log("redirect ngOnInit");
-	//this.router.navigate(['/home']);
-	}
-
-	ngOnDestroy(): void {
-		console.log("redirect ngOnDestroy");
-		this.oidcSecurityService.onModuleSetup.unsubscribe();
-	}
-
-	private doCallbackLogicIfRequired() {
-		console.log("redirect doCallbackLogicIfRequired");
-		if (window.location.hash) {
-			this.oidcSecurityService.authorizedCallback();
-		}
-	}
+  ngOnInit() {
+    this.oidcSecurityService.authorizedCallback();
+  }
 }
