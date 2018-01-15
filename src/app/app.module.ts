78de7eab0588268b75e086216be1f439c944ab08
Hendrik Wouters <hendrik.wouters@live.nl>
2018-01-15 20:27:52 +0100

    linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,25 +22,25 @@ export function loadConfig(oidcConfigService: OidcConfigService) {
 }
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'redirect.html', component: RedirectComponent }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'redirect.html', component: RedirectComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RedirectComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    AuthModule.forRoot(),
-    RouterModule.forRoot(appRoutes),
-  ],
-  providers: [
+    declarations: [
+        AppComponent,
+        RedirectComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        AuthModule.forRoot(),
+        RouterModule.forRoot(appRoutes),
+    ],
+    providers: [
         OidcConfigService,
         {
             provide: APP_INITIALIZER,
@@ -48,8 +48,8 @@ const appRoutes: Routes = [
             deps: [OidcConfigService],
             multi: true
         }
-  ],
-  bootstrap: [AppComponent]
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 
@@ -57,28 +57,27 @@ export class AppModule {
         private oidcSecurityService: OidcSecurityService,
         private oidcConfigService: OidcConfigService,
     ) {
-			this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+        this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
 
-			const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-			openIDImplicitFlowConfiguration.stsServer = 'https://login.microsoftonline.com/tfp/fabrikamb2c.onmicrosoft.com/b2c_1_susi/oauth2/v2.0/';
-			openIDImplicitFlowConfiguration.redirect_url = 'http://localhost:65328/redirect.html';
-			openIDImplicitFlowConfiguration.client_id = 'e760cab2-b9a1-4c0d-86fb-ff7084abd902';
-			openIDImplicitFlowConfiguration.response_type = 'id_token token';
-			openIDImplicitFlowConfiguration.scope = 'openid https://fabrikamb2c.onmicrosoft.com/demoapi/demo.read';
-			openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'http://localhost:65328';
-			openIDImplicitFlowConfiguration.post_login_route = '/home';
-			openIDImplicitFlowConfiguration.forbidden_route = '/home';
-			openIDImplicitFlowConfiguration.unauthorized_route = '/home';
-			openIDImplicitFlowConfiguration.auto_userinfo = false;
-			openIDImplicitFlowConfiguration.log_console_warning_active = true;
-			openIDImplicitFlowConfiguration.log_console_debug_active = !environment.production;
-			openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 30;
+            const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+            openIDImplicitFlowConfiguration.stsServer = 'https://login.microsoftonline.com/tfp/fabrikamb2c.onmicrosoft.com/b2c_1_susi/oauth2/v2.0/';
+            openIDImplicitFlowConfiguration.redirect_url = 'http://localhost:65328/redirect.html';
+            openIDImplicitFlowConfiguration.client_id = 'e760cab2-b9a1-4c0d-86fb-ff7084abd902';
+            openIDImplicitFlowConfiguration.response_type = 'id_token token';
+            openIDImplicitFlowConfiguration.scope = 'openid https://fabrikamb2c.onmicrosoft.com/demoapi/demo.read';
+            openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'http://localhost:65328';
+            openIDImplicitFlowConfiguration.post_login_route = '/home';
+            openIDImplicitFlowConfiguration.forbidden_route = '/home';
+            openIDImplicitFlowConfiguration.unauthorized_route = '/home';
+            openIDImplicitFlowConfiguration.auto_userinfo = false;
+            openIDImplicitFlowConfiguration.log_console_warning_active = true;
+            openIDImplicitFlowConfiguration.log_console_debug_active = !environment.production;
+            openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 30;
 
+            const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+            authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
 
-			const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-			authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
-
-			this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+            this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
 
         });
 
