c901859cfed881e16ce7e80dee592e8c625332b3
damienbod <damien_bod@hotmail.com>
2018-08-20 22:06:15 +0200

    Damienbod/update npm packages (#15)
    * updating angular-cli
    
    * rxjs update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
